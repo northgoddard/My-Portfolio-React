3a8ba72d05ba537e648ba416a9b495afb65eef7b
northgoddard <129123028+northgoddard@users.noreply.github.com>
2023-09-18 05:03:01 -0700

    resume link

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -11,6 +11,9 @@ function About() {
             <p>
                 I am a software developer from the upstate of South Carolina, with a passion for building applications that are both fun and new that can help solve a problem or present a solution for a growing need in the world. I have been coding for about 9 months and have a strong background in HTML, CSS, JavaScript, and React.
             </p>
+            <p>
+                Download my resume <a href="https://docs.google.com/document/d/1ts_saCN0zrvPcOyLerfk9oKeyG8b4LEoo_IBGbYdDb8/edit?usp=sharing">HERE.</a>
+            </p>
             </section>
         </div>
     )
